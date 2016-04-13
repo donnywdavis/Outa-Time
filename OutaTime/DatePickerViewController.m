d735b6cac3a5ed861a0d198d627d17231e90e5b0
Donny Davis <donnywdavis@icloud.com>
2016-04-13 18:48:25 -0400

    Add selection of important dates to DatePickerViewController and animate the date picker to that date

--- OutaTime/DatePickerViewController.m
+++ OutaTime/DatePickerViewController.m
@@ -12,6 +12,17 @@
 
 @property (weak, nonatomic) IBOutlet UIDatePicker *datePicker;
 
+@property (weak, nonatomic) NSString *independenceDay;
+@property (weak, nonatomic) NSString *kennedyAssassination;
+@property (weak, nonatomic) NSString *moonLanding;
+@property (weak, nonatomic) NSString *starWarsRelease;
+@property (weak, nonatomic) NSString *back2FutureRelease;
+@property (weak, nonatomic) NSString *fluxCapacitor;
+
+@property (strong, nonatomic) NSArray *importantDatesArray;
+
+- (IBAction)importantDateSelected:(UIButton *)sender;
+
 @end
 
 @implementation DatePickerViewController
@@ -20,6 +31,15 @@
 {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    
+    self.independenceDay = @"07/04/1776";
+    self.kennedyAssassination = @"11/22/1963";
+    self.moonLanding = @"07/20/1969";
+    self.starWarsRelease = @"05/25/1977";
+    self.back2FutureRelease = @"07/03/1985";
+    self.fluxCapacitor = @"11/12/1955";
+    
+    self.importantDatesArray = [[NSArray alloc] initWithObjects:self.starWarsRelease, self.independenceDay, self.kennedyAssassination, self.moonLanding, self.back2FutureRelease, self.fluxCapacitor, nil];
 }
 
 - (void)viewWillDisappear:(BOOL)animated
@@ -36,6 +56,20 @@
 {
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
+}
+
+- (IBAction)importantDateSelected:(UIButton *)sender {
+    // First let's create a date formatter
+    NSDateFormatter *dateFormatter = [[NSDateFormatter alloc] init];
+    // Let's set a format for our date
+    NSString *formatString = [NSDateFormatter dateFormatFromTemplate:@"MMddyyyy"
+                                                             options:0
+                                                              locale:[NSLocale currentLocale]];
+    [dateFormatter setDateFormat:formatString];
+    
+    NSDate *importantDate = [dateFormatter dateFromString:self.importantDatesArray[sender.tag]];
+    
+    [self.datePicker setDate:importantDate animated:true];
 }
 
 @end
